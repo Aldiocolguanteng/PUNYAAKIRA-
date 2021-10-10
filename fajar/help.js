00d189a37859739ca12a4cb247e70739128683be
Ciee Kepo Nama Gua Awokwok <86996031+Aldiocolguanteng@users.noreply.github.com>
2021-10-10 07:04:38 +0700

    Add files via upload

---
+++ fajar/help.js
@@ -0,0 +1,75 @@
+exports.menu = (pushname, sender, prefix, banChats, offline, ucapanWaktu, wib, wita, wit, prema, latensii, totalgrouppp, totalkontakkk, totalchattt, SadGril) => {
+return`${ucapanWaktu} ${pushname}
+
+*✗⃝🎩* _Creator_  *:* @${SadGril.split("@")[0]}
+*✗⃝🎩* _Hit Today_  *:* ${hit_today.length} _*Hit*_
+*✗⃝🎩* _Lib_ *:* _*Baileys*_
+*✗⃝🎩* _Type_ *:* _*NodeJs*_
+*✗⃝🎩* _Prefix_ *:* *『 _MultiPrefix_ 』*
+*✗⃝🎩* _Mode_ *:* ${banChats ? 'SELF' : 'PUBLIC'}
+*✗⃝🎩* _Status_ *:* ${offline ? 'OFFLINE' : 'ONLINE'}
+*✗⃝🎩* _User_ *:* ${prema}
+*✗⃝🎩* _Group Chat_ *:* ${totalgrouppp.length}
+*✗⃝🎩* _Private Chat_ *:* ${totalkontakkk.length}
+*✗⃝🎩* _Total Chat_ *:* ${totalchattt.length}
+*✗⃝🎩* _Speed_ *:* ${latensii.toFixed(4)} _*Seconds*_
+
+*『 _INFO TIME_ 』*
+✗⃝🌹 _${wib}_ _*WIB*_
+✗⃝🌹 _${wita}_ _*WITA*_
+✗⃝🌹 _${wit}_ _*WIT*_
+
+         !^_^!
+         
+${prefix}once
+${prefix}oncevid
+${prefix}bugtroli
+${prefix}bugtroli2
+${prefix}bugcombine
+${prefix}buglokasi
+${prefix}bugpc
+${prefix}bugpc2
+${prefix}bugvid
+${prefix}bugfoto
+${prefix}bugmusik
+${prefix}jadivilip
+${prefix}buginvit
+${prefix}troliv2
+${prefix}selayer
+${prefix}bugcata
+${prefix}bugkontak
+${prefix}bugs
+${prefix}bugvn
+${prefix}jadivirgam
+${prefix}bom
+${prefix}jadiselayer
+${prefix}sendbug
+
+${prefix}off <alasan>
+${prefix}on <alasan>
+${prefix}status
+${prefix}repeat <query>
+${prefix}clearall
+${prefix}rall
+${prefix}block
+${prefix}unblock
+${prefix}notif
+${prefix}unpin
+${prefix}pin
+${prefix}unreadall
+${prefix}unarchiveall
+${prefix}archive
+
+${prefix}upswteks <query>
+${prefix}upswimage <reply>
+${prefix}upswvideo <reply>
+
+${prefix}term <code>
+
+> <code>
+=> <code>
+$ <code>
+
+ꪶ𖣂ꫂKirBotzꪶ𖣂ꫂ
+`
+}
